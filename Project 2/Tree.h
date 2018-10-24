c53ecb2222283f33cc3d37e93d858a2833bcbcf6
Jorge Sepulveda <gsepulveda9697@gmail.com>
2018-10-24 22:51:28 +0000

    added tree folder and header

---
+++ Project 2/Tree.h
@@ -0,0 +1,19 @@
+#ifndef _DOUBLINGQUEUE_H
+#define _DOUBLINGQUEUE_H
+
+#include "Queue.h"
+
+//#define SIZE = 10
+
+#include <iostream>
+
+template <class Type>
+class DoublingQueue : public AbstractQueue<Type>{
+    private:
+    
+    public:
+       
+    
+};
+
+#endif
