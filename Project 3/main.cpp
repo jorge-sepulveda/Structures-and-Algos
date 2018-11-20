b56a40e725e7c1bae2a2f2f1d464d43b03e038f9
Jorge Sepulveda <gSepulveda9697@gmail.com>
2018-11-19 20:05:34 -0600

    CSV working

--- Project 3/main.cpp
+++ Project 3/main.cpp
@@ -17,54 +17,97 @@ int main(){
 
         srand((unsigned)time(0));
         int random_integer;
+        string s1,s2,s3;
+
+        ofstream logs;
+        logs.open("results.csv");
 
         CStopWatch whole; //initializing stopwatch instance
         UnsortedPQ<int> upq;
         SortedPQ<int> spq;
         HeapPQ<int> hpq;
 
+
+
         whole.Reset();
+        logs << "UnsortedPQ,";
         for (int i=1;i<=50000;i++){
             random_integer = (rand()%100)+1;
             upq.insert(random_integer);
+            if (i%50==0){
+                s1 = to_string(whole.GetElapsedSeconds());
+                logs << s1 << ",";
+            }
         }
+        logs << "\n";
         cout << to_string(whole.GetElapsedSeconds()) << "\n";
 
         whole.Reset();
-
+        logs << "SortedPQ,";
         for (int i=1;i<=50000;i++){
             random_integer = (rand()%100)+1;
             spq.insert(random_integer);
+            if (i%50==0){
+                s2 = to_string(whole.GetElapsedSeconds());
+                logs << s2 << ",";
+            }
         }
+        logs << "\n";
         cout << to_string(whole.GetElapsedSeconds()) << "\n";
         whole.Reset();
 
+        logs << "HeapPQ,";
         for (int i=1;i<=50000;i++){
             random_integer = (rand()%100)+1;
             hpq.insert(random_integer);
+            if (i%50==0){
+                s3 = to_string(whole.GetElapsedSeconds());
+                logs << s3 << ",";
+            }
         }
+        logs << "\n";
         cout << to_string(whole.GetElapsedSeconds()) << "\n";
+        logs << "\n";
 
         ///////////////////////////////////////////////
+        logs << "UnsortedPQ Removal,";
         whole.Reset();
-
         for (int i=1;i<=25000;i++){
             upq.removeMin();
+            if (i%50==0){
+                s1 = to_string(whole.GetElapsedSeconds());
+                logs << s1 << ",";
+            }
         }
+        logs << "\n";
         cout << to_string(whole.GetElapsedSeconds()) << "\n";
 
+        logs << "SortedPQ Removal,";
         whole.Reset();
-
         for (int i=1;i<=25000;i++){
             spq.removeMin();
+            if (i%50==0){
+                s2 = to_string(whole.GetElapsedSeconds());
+                logs << s2 << ",";
+            }
         }
+        logs << "\n";
         cout << to_string(whole.GetElapsedSeconds()) << "\n";
-        whole.Reset();
 
+
+        logs << "HeapPQ Removal,";
+        whole.Reset();
         for (int i=1;i<=25000;i++){
             hpq.removeMin();
+            if (i%50==0){
+                s3 = to_string(whole.GetElapsedSeconds());
+                logs << s3 << ",";
+            }
         }
+        logs << "\n";
         cout << to_string(whole.GetElapsedSeconds()) << "\n";
+
+        logs.close();
 
         //HeapPQ<int> hpq;
 
