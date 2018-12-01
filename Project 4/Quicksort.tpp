2a533cd6a0c6022e4305b90e91343e2147488323
Jorge Sepulveda <gSepulveda9697@gmail.com>
2018-12-01 12:17:51 -0600

    qsort working, timer added

--- Project 4/Quicksort.tpp
+++ Project 4/Quicksort.tpp
@@ -2,19 +2,41 @@
 
 using namespace std;
 
+
+template <class T>
+void Swap(T& a, T& b){
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
 template <class T>
 int partition(T* arr, int low, int high){
 
     srand((unsigned)time(0));
 
-    int l = low;
-    int h = high;
-    
-    T temp;
+    int p = (rand()%(high-low + 1) + low);
 
-    T pivot = arr[(rand()%(high-low + 1) + low)];
+    Swap(arr[p], arr[low]);
+    T pivot = arr[low];
+    int left = low;
 
+    for (int i = (low+1); i <= high;i++){
+        if (arr[i] < pivot){
+            left +=1;
+            Swap(arr[i], arr[left]);
+        }
+    }
+    Swap(arr[low], arr[left]);
+    return left;
 
+}
 
-
+template <class T>
+void Qsort(T* arr, int first, int last){
+    if (first < last){
+        int pivot = partition(arr, first, last);
+        Qsort(arr, first, pivot-1);
+        Qsort(arr, pivot+1, last);
+    }
 }
--- Project 4/main.cpp
+++ Project 4/main.cpp
@@ -6,6 +6,7 @@
 #include <fstream>
 #include "Mergesort.tpp"
 #include "Quicksort.tpp"
+#include "stopwatch.h"
 
 using namespace std;
 
@@ -15,23 +16,61 @@ int main(int argc, char const *argv[]) {
     int random_integer;
     double random_double;
 
+    CStopWatch timer;
 
     unsigned int size = 1000000;
-    double* passArray = new double[1000000];
+
+    int* qIntArray = new int[1000000];
+    int* mIntArray = new int[1000000];
+
+    double* qDoubleArray = new double[1000000];
+    double* mDoubleArray = new double[1000000];
+
+
 
     for (int i=0;i<size;i++){
         random_double = ((double) rand() / (RAND_MAX));
-        passArray[i] = random_double;
-        //cout << "->" << passArray[i];
+        qDoubleArray[i] = random_double;
+        mDoubleArray[i] = random_double;
     }
-    cout << "\n";
-    mergesort(passArray, 0, size-1);
-    cout << "sorted!" << endl;
-    /*for (int i=0;i<size;i++){
-        cout << "->" << passArray[i];
-    }*/
-    cout << "\n";
 
-    delete[] passArray;
+    for (int i = 0; i<size;i++){
+        random_integer = (rand()%10)+1;
+        qIntArray[i] = random_integer;
+        mIntArray[i] = random_integer;
+    }
+
+    timer.Reset();
+    Qsort(qDoubleArray, 0, size-1);
+    cout << "Double Quicksort time: ";
+    cout << to_string(timer.GetElapsedSeconds()) << endl;
+
+    timer.Reset();
+    mergesort(mDoubleArray, 0, size-1);
+    cout << "Double Mergesort time: ";
+    cout << to_string(timer.GetElapsedSeconds()) << endl;
+
+    timer.Reset();
+    Qsort(qIntArray, 0, size-1);
+    cout << "Int Quicksort time: ";
+    cout << to_string(timer.GetElapsedSeconds()) << endl;
+
+    timer.Reset();
+    mergesort(mIntArray, 0, size-1);
+    cout << "int Mergesort time: ";
+    cout << to_string(timer.GetElapsedSeconds()) << endl;
+
+
+    /*for (int i=0;i<intSize;i++){
+
+        random_integer = (rand()%100)+1;
+        intArray[i] = random_integer;
+    }*/
+
+    delete[] qIntArray;
+    delete[] mIntArray;
+    delete[] mDoubleArray;
+    delete[] qDoubleArray;
+
     return 0;
 }
